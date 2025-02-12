0476eaa68c6fbb68700bfcef56bf604726afb5f2
Charles Corredor <charlescreativo@gmail.com>
2025-02-12 15:12:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function ShowHide(){
+    var container_1 = document.getElementsByClassName("container_1")[0];
+    
+    if (
+    container_1.style.visibility == "hidden"
+    ){
+        container_1.style.visibility = "visible";
+
+}else{
+    container_1.style.visibility = "hidden";
+}
+}
+
+function ShowHide_2(){
+    var container_2 = document.getElementsByClassName("container_2")[0];
+    
+    if (
+    container_2.style.visibility == "hidden"
+    ){
+        container_2.style.visibility = "visible";
+
+}else{
+    container_2.style.visibility = "hidden";
+}
+}
